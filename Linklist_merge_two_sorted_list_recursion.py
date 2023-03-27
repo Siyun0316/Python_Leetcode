91542df8e1ded0cf385e1eed4a743fa9319f10a8
Siyun316 <xsiyun@gmail.com>
2023-03-27 11:24:15 -0500

    # of problem: 3

--- Linklist_merge_two_sorted_list_recursion.py
+++ Linklist_merge_two_sorted_list_recursion.py
@@ -13,6 +13,30 @@ class Listnode(object):
         self.val = val
         self.next = None
 
+
+class Linklist(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def __str__(self):
+        strs = []
+        node = self.head
+        while node:
+            strs.append(str(node.val))
+            node = node.next
+        return "->".join(strs)
+
+    def append(self, val):
+        if not self.head:
+            self.head = self.tail = Listnode(val)
+            return
+        newnode = Listnode(val)
+        self.tail.next = newnode
+        self.tail = self.tail.next
+        return
+
+
 class Solution(object):
     def merge(self,lh,rh):
         if not lh:
@@ -28,3 +52,27 @@ class Solution(object):
 # Time complexity O(len(lh) + len(rh))
 
 # Space complexity O(len(lh) + len(rh))
+
+#test
+'''
+test_a = Linklist()
+test_b = Linklist()
+test_c = Linklist()
+
+for i in range(10):
+    test_b.append(i*2+1)
+    test_c.append(i+3)
+print(test_a)
+print(test_b)
+print(test_c)
+a = Solution().merge(test_b.head,test_a.head)
+sorted_list2 = Linklist()
+sorted_list2.head = a
+print(sorted_list2)
+c = Solution().merge(test_b.head,test_c.head)
+sorted_list1 = Linklist()
+sorted_list1.head = c
+print(sorted_list1)
+# inplace edit the input linkedlist 
+print(sorted_list2)
+'''
