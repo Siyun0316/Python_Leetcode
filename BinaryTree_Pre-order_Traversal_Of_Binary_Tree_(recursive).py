2a1bd069216dd2e1594bbe613bb8ce1410c60542
Siyun316 <xsiyun@gmail.com>
2023-04-09 19:34:25 -0500

    # of problem: 51

---
+++ BinaryTree_Pre-order_Traversal_Of_Binary_Tree_(recursive).py
@@ -0,0 +1,68 @@
+# 655. Pre-order Traversal Of Binary Tree (recursive)
+# Implement a recursive, pre-order traversal of a given binary tree,
+# return the list of keys of each node in the tree as it is pre-order traversed.
+class TreeNode(object):
+    def __init__(self,val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+class Solution(object):
+    def preOrder(self,root):
+        """
+        input: TreeNode root
+        return: Integer[]
+        """
+        if not root:
+            return []
+        res = []
+        res.append(root.val)
+        res += self.preOrder(root.left)
+        res += self.preOrder(root.right)
+        return res
+
+# time complexity O(n)
+# space complexity O(n)
+
+# Test case 1
+#      1
+#     / \
+#    2   3
+# Output: [2, 1, 3]
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+print(Solution().preOrder(root))
+
+
+# Test case 2
+#      1
+#       \
+#        2
+# Output: [1, 2]
+
+root = TreeNode(1)
+root.right = TreeNode(2)
+print(Solution().preOrder(root))
+
+
+# Test case 3
+#      1
+#     / \
+#    2   3
+#       / \
+#      4   5
+# Output: [2, 1, 4, 3, 5]
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.right.left = TreeNode(4)
+root.right.right = TreeNode(5)
+print(Solution().preOrder(root))
+
+# Test case 4
+# Output: []
+
+print(Solution().preOrder(None))
